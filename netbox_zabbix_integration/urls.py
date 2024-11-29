23ff15e32d6874b99f7e23dba126b036aea9f74b
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-11-29 18:46:24 +0330

    fixed reverse

--- netbox_zabbix_integration/urls.py
+++ netbox_zabbix_integration/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'netbox_zabbix_integration'
+
 urlpatterns = (
     path('relationship/', views.RelationshipListView.as_view(), name='relationship_list'),
     path('relationship/<int:pk>/', views.RelationshipView.as_view(), name='relationship'),
