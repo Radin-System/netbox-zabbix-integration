48122f450407f5a55fcaad938657fce3dd355a69
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-11-29 22:05:32 +0330

    Improvements

--- netbox_zabbix_integration/views/zabbix_status.py
+++ netbox_zabbix_integration/views/zabbix_status.py
@@ -1,27 +1,43 @@
-# views.py
 from django.views.generic import View
 from netbox.plugins import get_plugin_config
 from django.shortcuts import render
+
+from netbox_zabbix_integration.zabbix_api import ZabbixAPIManager
 
 class ZabbixStatusView(View):
     template_name = 'netbox_zabbix_integration/zabbix-status.html'
 
     def get(self, request, *args, **kwargs):
         # Retrieve configuration settings
-        zabbix_url = get_plugin_config('netbox_zabbix_integration', 'url')
-        zabbix_token = get_plugin_config('netbox_zabbix_integration', 'token')
+        url = get_plugin_config('netbox_zabbix_integration', 'url')
+        token = get_plugin_config('netbox_zabbix_integration', 'token')
         verify_ssl = get_plugin_config('netbox_zabbix_integration', 'verify_ssl')
-
-        # Test connection (basic example)
+        username = get_plugin_config('netbox_zabbix_integration', 'username')
         connection_status = "Unknown"
         error_message = None
+        auth_mode = 'UNKHOWN'
+
+        if token is not None:
+            auth_mode = 'Token'
+        elif username is not None:
+            auth_mode = 'Username'
+
+        try:
+            zapi = ZabbixAPIManager.get_instance()
+            api_version = str(zapi.api_version())
+            connection_status = 'Online'
+            zapi.login()
+        except Exception as e:
+            connection_status = 'Offline'
+            error_message = str(e)
 
         # Context for rendering the template
         context = {
-            'zabbix_url': zabbix_url,
+            'zabbix_url': url,
             'verify_ssl': verify_ssl,
             'connection_status': connection_status,
             'error_message': error_message,
-
+            'auth_mode': auth_mode,
+            'api_version': api_version,
         }
         return render(request, self.template_name, context)
