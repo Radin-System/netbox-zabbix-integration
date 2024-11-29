d899db326fcad8e7a774f2f99ae5d1b242b1abc3
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-11-29 21:18:31 +0330

    asd

--- netbox_zabbix_integration/urls.py
+++ netbox_zabbix_integration/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import relationship
+from .views import *
 
 app_name = 'zabbix_integration'
 
 urlpatterns = (
-    path('zabbix-status/', relationship.ZabbixStatusView.as_view(), name='zabbix-status'),
+    path('zabbix-status/', ZabbixStatusView.as_view(), name='zabbix-status'),
 
-    path('relationship/', relationship.RelationshipListView.as_view(), name='relationship_list'),
-    path('relationship/<int:pk>/', relationship.RelationshipView.as_view(), name='relationship'),
+    path('relationship/', RelationshipListView.as_view(), name='relationship_list'),
+    path('relationship/<int:pk>/', RelationshipView.as_view(), name='relationship'),
 )
