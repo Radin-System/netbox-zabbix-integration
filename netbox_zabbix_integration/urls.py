8900270c8f239efcd274a2c6397689b37dcccb12
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-11-29 17:51:39 +0330

    Fix

--- netbox_zabbix_integration/urls.py
+++ netbox_zabbix_integration/urls.py
@@ -3,12 +3,12 @@ from netbox.views.generic import ObjectChangeLogView
 from . import models, views
 
 urlpatterns = (
-    path('zabbix-integration/', views.ZabbixRelationshipListView.as_view(), name='zabbix_relationship_list'),
-    path('zabbix-integration/add/', views.ZabbixRelationshipEditView.as_view(), name='zabbix_relationship_add'),
-    path('zabbix-integration/<int:pk>/', views.ZabbixRelationshipView.as_view(), name='zabbix_relationship'),
-    path('zabbix-integration/<int:pk>/edit/', views.ZabbixRelationshipEditView.as_view(), name='zabbix_relationship_edit'),
-    path('zabbix-integration/<int:pk>/delete/', views.ZabbixRelationshipDeleteView.as_view(), name='zabbix_relationship_delete'),
-    path('zabbix-integration/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='zabbix_relationship_changelog', kwargs={
+    path('zabbix-relationship/', views.ZabbixRelationshipListView.as_view(), name='zabbix_relationship_list'),
+    path('zabbix-relationship/add/', views.ZabbixRelationshipEditView.as_view(), name='zabbix_relationship_add'),
+    path('zabbix-relationship/<int:pk>/', views.ZabbixRelationshipView.as_view(), name='zabbix_relationship'),
+    path('zabbix-relationship/<int:pk>/edit/', views.ZabbixRelationshipEditView.as_view(), name='zabbix_relationship_edit'),
+    path('zabbix-relationship/<int:pk>/delete/', views.ZabbixRelationshipDeleteView.as_view(), name='zabbix_relationship_delete'),
+    path('zabbix-relationship/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='zabbix_relationship_changelog', kwargs={
         'model': models.ZabbixRelationship
     }),
 )
